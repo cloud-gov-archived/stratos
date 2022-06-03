0fa30f58143be4c4ce478263ea3661ca5111c391
rcgottlieb <robert.gottlieb@gsa.gov>
2022-06-03 14:08:15 -0400

    Need to add a specific version to get the latest of a go package

--- build/bk-build.sh
+++ build/bk-build.sh
@@ -34,7 +34,7 @@ go env
 
 # Need to install swag in both cases
 echo "Generating OpenAPI documentation..."
-go get github.com/swaggo/swag/cmd/swag
+go get github.com/swaggo/swag/cmd/swag@1.8.2
 swag init
 
 if [ "${ACTION}" == "build" ]; then
